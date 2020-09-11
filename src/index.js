300dd7a0d6492da1233ba2f5d730285af4fb7c86
Dhananjay09 <dj091298@gmail.com>
2020-09-11 22:30:50 +0530

    Final Commit trying

--- src/index.js
+++ src/index.js
@@ -16,7 +16,27 @@ app.use(bodyParser.json())
 app.get("/",(req, res)=>{
     return res.json("Hello World!")
 })
+app.post("/add", (req, res)=>{
+    let {num1,num2}=req.body;
+    if(typeof(num1)==="string" || typeof(num2)==="string"){
+        return res.json({
+            message: "invalid data types"
+        })
+    }
+    num1=parseFloat(num1)
+    num2=parseFloat(num2)
+    if(num1 > parseFloat(1000000) || num2 > parseFloat(1000000)){
+        return res.json({
+            message: "Overflow"
+        })
+    }
+    return res.json({
+        status: "success/failure/error",
+        message: "the sum of given two number",
+        sum: num1+num2
+    })
 
+})
 // here
 //app.use("",Mainpage)
 
