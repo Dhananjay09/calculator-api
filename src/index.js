4d4e18ce31571da6af98fe86de334714f22c1d33
Dhananjay09 <dj091298@gmail.com>
2020-09-11 08:40:04 +0530

    First Update by Dhananjay Singh

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Mainpage=require('./route')
 const app = express()
 const bodyParser = require("body-parser");
 const port = 3000
@@ -12,9 +13,82 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 // your code goes here
+app.get("/",(req, res)=>{
+    return res.json("Hello World!")
+})
+app.post("/add", (req, res)=>{
+    const {num1, num2}=req.body;
+    if(typeof (num1)=="string" || typeof (num2)=="string"){
+        return res.json({
+            message: "invalid data types"
+        })
+    }
+    if(num1 >1000000 || num2 > 1000000){
+        return res.json({
+            message: "Overflow"
 
+        })
+    }
+    return res.json({
+        status: "success/failure/error",
+        message: "the sum of given two number",
+        sum: num1+num2
+    })
+})
+app.post("/sub", (req, res)=>{
+    const {num1, num2}=req.body;
+    if(typeof (num1)=="string" || typeof (num2)=="string"){
+        return res.json({
+            message: "invalid data types"
+        })
+    }
+    if(num1 >1000000 || num2 > 1000000){
+        return res.json({
+            message: "Underflow"
+
+        })
+    }
+    return res.json({
+        status: "success/failure/error",
+        message: "the difference of given two number",
+        sum: num1-num2
+    })
+})
+app.post("/multiply", (req, res)=>{
+    const {num1, num2}=req.body;
+    if(typeof (num1)=="string" || typeof (num2)=="string"){
+        return res.json({
+            message: "invalid data types"
+        })
+    }
+    if(num1 >1000000 || num2 > 1000000){
+        return res.json({
+            message: "Overflow"
+
+        })
+    }
+    return res.json({
+        status: "success/failure/error",
+        message: "The product of given numbers",
+        sum: num1*num2
+    })
+})
+app.post("/division", (req, res)=>{
+    const {num1, num2}=req.body;
+    if(num2==0){
+        return res.json({
+            message: "Cannot divide by zero" 
+        })
+    }
+    
+    return res.json({
+        status: "success/failure/error",
+        message: "the sum of given two number",
+        sum: num1+num2
+    })
+})
 // here
-
+//app.use("",Mainpage)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
