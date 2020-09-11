305022b4efc4163c29cf2fdc4f3f727be058638a
Dhananjay09 <dj091298@gmail.com>
2020-09-11 23:04:44 +0530

    Final Commit trying

--- src/index.js
+++ src/index.js
@@ -60,6 +60,28 @@ app.post("/sub", (req, res)=>{
     })
 
 })
+app.post("/multiply", (req, res)=>{
+    let {num1,num2}=req.body;
+    if(typeof(num1)=="string" || typeof(num2)=="string"){
+        return res.json({
+            message: "Invalid data types"
+        })
+    }
+    num1=parseFloat(num1)
+    num2=parseFloat(num2)
+    let s=num1*num2;
+    if(num1 > parseFloat(1000000) || num2 > parseFloat(1000000) || s > parseFloat(1000000)){
+        return res.json({
+            message: "Overflow"
+        })
+    }
+    return res.json({
+        status: "success/failure/error",
+        message: "The product of given numbers",
+        sum: s
+    })
+
+})
 // here
 //app.use("",Mainpage)
 
