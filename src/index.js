0ddc52411a78d0890f4dd572628a3de6df173b0a
Dhananjay09 <dj091298@gmail.com>
2020-09-12 09:45:08 +0530

    Final update done

--- src/index.js
+++ src/index.js
@@ -13,86 +13,92 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 // your code goes here
-app.get("/",(req, res)=>{
-    return res.json("Hello World!")
-})
-app.post("/add", (req, res)=>{
-    let {num1,num2}=req.body;
-    if(typeof(num1)=="string" || typeof(num2)=="string"){
+const checkforSTring=(req, res, next)=>{
+    const {num1,num2}=req.body;
+    if( typeof num1 ==="string" || typeof num2=="string"){
         return res.json({
+            status: `error`,
+			message: `Invalid data types`,
+        })
+    }
+    if(isNaN(num1) || isNaN(num2)){
+        return res.json({
+            status : "error",
             message: "Invalid data types"
         })
     }
+    next();
+}
+app.get("/",(req, res)=>{
+    return res.json("Hello World!")
+})
+app.post("/add", checkforSTring, (req, res)=>{
+    let {num1,num2}=req.body;
     num1=parseFloat(num1)
     num2=parseFloat(num2)
     let s=num1+num2;
-    if(num1 > parseFloat(1000000) || num2 > parseFloat(1000000) || s > parseFloat(1000000)){
+    if( s > parseFloat(1000000)){
         return res.json({
+            status: "error",
             message: "Overflow"
         })
     }
     return res.json({
-        status: "success/failure/error",
+        status: "success",
         message: "the sum of given two numbers",
         sum: s
     })
 
 })
-app.post("/sub", (req, res)=>{
+app.post("/sub", checkforSTring,(req, res)=>{
     let {num1,num2}=req.body;
-    if(typeof(num1)=="string" || typeof(num2)=="string"){
-        return res.json({
-            message: "Invalid data types"
-        })
-    }
     num1=parseFloat(num1)
     num2=parseFloat(num2)
     let s=num1-num2;
-    if(num1 < parseFloat(1000000)  || num2 < parseFloat(1000000) || s < parseFloat(1000000)) {
+    if( s < parseFloat(-1000000)) {
         return res.json({
+            status:"error",
             message: "Underflow"
         })
     }
     return res.json({
-        status: "success/failure/error",
+        status: "success",
         message: "the difference of given two numbers",
         sum: s
     })
 
 })
-app.post("/multiply", (req, res)=>{
+app.post("/multiply", checkforSTring, (req, res)=>{
     let {num1,num2}=req.body;
-    if(typeof(num1)=="string" || typeof(num2)=="string"){
-        return res.json({
-            message: "Invalid data types"
-        })
-    }
     num1=parseFloat(num1)
     num2=parseFloat(num2)
     let s=num1*num2;
-    if(num1 > parseFloat(1000000) || num2 > parseFloat(1000000) || s > parseFloat(1000000)){
+    if( s > parseFloat(1000000)){
         return res.json({
+            status: "error",
             message: "Overflow"
+
         })
     }
     return res.json({
-        status: "success/failure/error",
+        status: "success",
         message: "The product of given numbers",
         sum: s
     })
 
 })
-app.post("/division", (req, res)=>{
+app.post("/division",checkforSTring, (req, res)=>{
     let {num1,num2}=req.body;
     if( num2 === 0 ){
         return res.json({
+            status: "error",
             message: "Cannot divide by zero"
         })
     }
     return res.json({
-        status: "success/failure/error",
+        status: "success",
         message: "The division of given numbers",
-        sum: num1/num2
+        sum: parseFloat(num1/num2)
     })
 
 })
